d2b6a9eaf560d3b5aba13e810baa12a486043a92
limzh <123>
2020-08-17 14:48:31 +0800

    Deleted some comments

--- api/Login_first.py
+++ api/Login_first.py
@@ -44,25 +44,4 @@ def api_login():
 #Full link: /api/api_login/apilogin
 
 
-# user_login_toinfotest_api = Blueprint('user_login_toinfotest_api', __name__)
-
-# @user_login_toinfotest_api.route('/logintocheck', methods=['POST'])
-# def logintocheck():
-#     if request.is_json:
-#         username = request.json['username']
-#         password = request.json['password']
-#     else:
-#         username = request.form['username']
-#         password = request.form['password']
-#
-#
-#     user = User.query.filter_by(username=username, password=password).first()
-#     print(user)
-#     if user:
-#         access_token = create_access_token(identity=username)
-#         return jsonify(message="Login successfully", access_token=access_token)
-#     else:
-#         return jsonify(message="Incorrect username or password"),401
-
-
 
